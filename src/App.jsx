e235335ce8a75695a6b6665ade5ba272c40b4568
Jisang Hong <jisang1024@gmail.com>
2022-09-01 14:55:00 +0900

    create number space

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,17 @@
 import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import "./App.css";
+import Number from "./Number.jsx";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <div className="container">
       <div className="result">result</div>
       <div className="backspace">backspace</div>
       <div className="clear">clear</div>
-      <div className="number">number</div>
+      <div className="number">
+        <Number />
+      </div>
       <div className="sign">sign</div>
     </div>
   );
---
+++ src/Number.jsx
@@ -0,0 +1,20 @@
+import "./Number.css";
+
+export default function Number() {
+  return (
+    <div className="number">
+      <div className="9">9</div>
+      <div className="8">8</div>
+      <div className="7">7</div>
+      <div className="6">6</div>
+      <div className="5">5</div>
+      <div className="4">4</div>
+      <div className="3">3</div>
+      <div className="2">2</div>
+      <div className="1">1</div>
+      <div className="0">0</div>
+      <div className="00">00</div>
+      <div className=".">.</div>
+    </div>
+  );
+}
